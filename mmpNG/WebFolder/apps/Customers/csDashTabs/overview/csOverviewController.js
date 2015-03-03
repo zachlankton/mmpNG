963d6c235e5cb03605dd6f4c4ee2a849677eb7c5
Zach Lankton <zach@atrecon.com>
2015-03-03 06:09:13 -0500

    Added Customer Detail Form

--- mmpNG/WebFolder/apps/Customers/csDashTabs/overview/csOverviewController.js
+++ mmpNG/WebFolder/apps/Customers/csDashTabs/overview/csOverviewController.js
@@ -5,8 +5,15 @@ myApp.controller('csOverviewController', function ($scope, $wakanda, $filter, cs
 	var csOverview = rScope.csOverview = {};
 
 	
+	var c = rScope.Customers.currentSelection;
    	var csFields = csOverview.csFields = [];
-   	csFields.push({title:"Customer Name", model: ""});
+   	csFields.push({title:"Customer Name", type:"text", name: "name" });
+   	csFields.push({title:"Product Type", type:"text", name: "productTypeRef"});
+   	csFields.push({title:"EIN No", type:"text", name: "einNo"});
+   	csFields.push({title:"Terms", type:"text", name: "terms"});
+   	csFields.push({title:"Representative", type:"text", name: "representative"});
+   	csFields.push({title:"Notes", type:"textarea", name: "notes"});
+
 			
   
 });
