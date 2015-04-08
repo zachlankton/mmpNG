efeb66f136587879f4aabaf58e67a6c624b2d69f
Zach Lankton <zach@atrecon.com>
2015-04-07 21:29:05 -0400

    Eliminated the last function in orders controller!

--- mmpNG/WebFolder/apps/Customers/csDashTabs/orders/csOrdersController.js
+++ mmpNG/WebFolder/apps/Customers/csDashTabs/orders/csOrdersController.js
@@ -3,25 +3,6 @@ myApp.controller('csOrdersController', function ($scope, $wakanda, $filter, csAp
 	$scope.csOrders = csAppData.getData();
 	var rScope = $scope.csOrders;
 	var csOrders = rScope.csOrders = {};
-	csOrders.orderSearch = "";
-	csOrders.spOrderSearch = "";
-
-
-	/////////////////////////////////
-    // ADD RELATED SUPPLIER PO     //
-    /////////////////////////////////
-	csOrders.addSpOrder = function(){
-	    var curOrder = rScope.current.Orders;
-		
-		var newEntity = $wakanda.$ds.PurchaseOrders.$create({
-		        csOrder: curOrder
-            });
-
-		newEntity.$save().then(function(e) {
-		    rScope.collections.PurchaseOrders.push(newEntity);
-			rScope.current.PurchaseOrders = newEntity;
-		})
-	};
 
 
 
