ec2f14f876a0c71586a7f102e2bdc11a147e40b7
Zach Lankton <zach@atrecon.com>
2015-04-16 22:40:33 -0400

    updated orderLineItem Model to get Quoted Prices based on ordered qty.

--- mmpNG/Model/OrderLineItems/OrderLineItems-events.js
+++ mmpNG/Model/OrderLineItems/OrderLineItems-events.js
@@ -8,6 +8,38 @@ model.OrderLineItems.modified.onGet = function() {
 model.OrderLineItems.modified.onSet = function(value) {
 	return "";
 };
+
+
+
+
 model.OrderLineItems.events.remove = function(event) {
 	this.orderLineItemQtysCollection.remove();
+};
+
+
+model.OrderLineItems.quotedPrice.onGet = function() {
+	if (this.partRev == undefined) {return "";}
+	if (this.partRev.quoteLineRef == undefined) {return "No Quote";}
+	var priceBreaks = this.partRev.quoteLineRef.csQuoteLineItemQtysCollection.orderBy('qty');
+	var totalQty = this.orderLineItemQtysCollection.sum("qty");
+	if (totalQty == 0){return "";}
+	var priceBreak = priceBreaks.query("qty <= " + totalQty).orderBy("qty desc");
+	if (priceBreak[0] == undefined){
+		return "Min Qty " + priceBreaks[0].qty;
+	}else{
+		return priceBreak[0].price;
+	}
+};
+
+model.OrderLineItems.quotedPrice.onSet = function() {
+	return "";
+};
+
+
+model.OrderLineItems.totalQty.onGet = function() {
+	return this.orderLineItemQtysCollection.sum("qty");
+};
+
+model.OrderLineItems.totalQty.onSet = function() {
+	return "";
 };
