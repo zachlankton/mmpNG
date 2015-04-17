72ef350bb0b3768ad6a9e2ed5e28f08e7591a121
Zach Lankton <zach@atrecon.com>
2015-04-16 22:41:07 -0400

    updated orders form to refresh quotedPrice on qty changes

--- mmpNG/WebFolder/apps/Customers/csDashTabs/orders/csOrdersController.js
+++ mmpNG/WebFolder/apps/Customers/csDashTabs/orders/csOrdersController.js
@@ -5,5 +5,17 @@ myApp.controller('csOrdersController', function ($scope, $wakanda, $filter, csAp
 	var csOrders = rScope.csOrders = {};
 
 
+    csOrders.saveQty = function(params){
+        if (params.length == 1){
+            params[0].$save(); //update quotedPrice
+            return 0;
+        }
+        var oLine = params[0];
+        var oliq = params[1];
+        var saveFn = params[2];
+
+        saveFn(oliq);
+        oLine.$save();
+    }
 
 });
