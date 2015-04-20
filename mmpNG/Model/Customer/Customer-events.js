e52dbc20c618845c2a876c9cc24411fd46fbd800
Zach Lankton <zach@atrecon.com>
2015-04-20 18:56:16 -0400

    Made Part Numbers searchable through customers

--- mmpNG/Model/Customer/Customer-events.js
+++ mmpNG/Model/Customer/Customer-events.js
@@ -18,3 +18,20 @@ model.Customer.events.remove = function(event) {
 	this.csAddressCollection.remove();
 	this.supplierQuotesCollection.remove();
 };
+
+
+model.Customer.parts.onGet = function() {
+	var parts = this.partNumberCollection;
+	if (parts === undefined){return "";}
+	
+	var partsStr = "";
+	parts.forEach(function(part){
+		partsStr = partsStr + part.partNo + ",";
+	});
+	return partsStr;
+};
+
+
+model.Customer.parts.onSet = function() {
+	return "";
+};
